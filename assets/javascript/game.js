008e33df9406704b2556a542645fb9b651fdfb1b
Timothy Na <runy385@gmail.com>
2018-08-25 18:04:39 -0400

    finished product

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,8 +1,8 @@
 "use strict";
 var shownNumber = 0;
 var totalSumNum = 0;
-var wins = parseInt(0);
-var losses = parseInt(0);
+var wins = 0;
+var losses = 0;
 var hasFinished = false;
 var crystal1 = 0;
 var crystal2 = 0;
@@ -19,54 +19,52 @@ function resetGame() {
     totalSumNum = 0;
 };
 
+
+$("#crystal1").on("click", function () {
+    totalSumNum += crystal1;
+    console.log("totalSumNum after change " + totalSumNum);
+    document.getElementById("totalScore").innerText = totalSumNum;
+    checkWin();
+});
+
+$("#crystal2").on("click", function () {
+    totalSumNum += crystal2;
+    console.log("totalSumNum after change " + totalSumNum);
+    document.getElementById("totalScore").innerText = totalSumNum;
+    checkWin();
+});
+
+$("#crystal3").on("click", function () {
+    totalSumNum += crystal3;
+    console.log("totalSumNum after change " + totalSumNum);
+    document.getElementById("totalScore").innerText = totalSumNum;
+    checkWin();
+});
+
+$("#crystal4").on("click", function () {
+    totalSumNum += crystal4;
+    console.log("totalSumNum after change " + totalSumNum);
+    document.getElementById("totalScore").innerText = totalSumNum;
+    checkWin();
+});
+
 function updateDisplay() {
     console.log(shownNumber);
     document.getElementById("displayObjectiveNum").innerHTML = "\n" + shownNumber;
-
+    
     crystal1 = Math.floor(Math.random() * 12) + 1;
     crystal2 = Math.floor(Math.random() * 12) + 1;
     crystal3 = Math.floor(Math.random() * 12) + 1;
     crystal4 = Math.floor(Math.random() * 12) + 1;
     console.log(crystal1, crystal2, crystal3, crystal4);
-// values double when in 2nd phase of game
-// when points are added, they dont add correctly.
-
-    $("#crystal1").on("click", function () {
-        totalSumNum += crystal1;
-        console.log("totalSumNum after change " + totalSumNum);
-        document.getElementById("totalScore").innerText = totalSumNum;
-        checkWin();
-    });
-
-    $("#crystal2").on("click", function () {
-        totalSumNum += crystal2;
-        console.log("totalSumNum after change " + totalSumNum);
-        document.getElementById("totalScore").innerText = totalSumNum;
-        checkWin();
-    });
-
-    $("#crystal3").on("click", function () {
-        totalSumNum += crystal3;
-        console.log("totalSumNum after change " + totalSumNum);
-        document.getElementById("totalScore").innerText = totalSumNum;
-        checkWin();
-    });
-
-    $("#crystal4").on("click", function () {
-        totalSumNum += crystal4;
-        console.log("totalSumNum after change " + totalSumNum);
-        document.getElementById("totalScore").innerText = totalSumNum;
-        checkWin();
-    });
-
-
 
     console.log("totalSumNum b4 change " + totalSumNum);
 };
 
 function checkWin() {
     if (totalSumNum === shownNumber) {
-        document.getElementById("wins").textContent += 1;
+        wins++;
+        document.getElementById("wins").textContent = wins;
         hasFinished = true;
         checkEndGame();
     }
@@ -75,7 +73,8 @@ function checkWin() {
 
 function checkLoss() {
     if (totalSumNum > shownNumber) {
-        document.getElementById("losses").textContent += 1;
+        losses++;
+        document.getElementById("losses").textContent = losses;
         hasFinished = true;
     }
     checkEndGame();
